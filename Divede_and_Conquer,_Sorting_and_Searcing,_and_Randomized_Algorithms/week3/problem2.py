c6d08851f9de78b0a8e6edf000c990a8df8fcc83
bamboosteam <t.t.yeah.b5@gmail.com>
2021-06-22 08:53:25 +0900

    success in last pivot

--- Divede_and_Conquer,_Sorting_and_Searcing,_and_Randomized_Algorithms/week3/problem2.py
+++ Divede_and_Conquer,_Sorting_and_Searcing,_and_Randomized_Algorithms/week3/problem2.py
@@ -8,3 +8,45 @@
 # Compute the number of comparisons (as in Problem 1), always using the final element of the given array as the pivot element.  Again, be sure to implement the Partition subroutine exactly as it is described in the video lectures.
 #
 # Recall from the lectures that, just before the main Partition subroutine, you should exchange the pivot element (i.e., the last element) with the first element.
+
+
+def txt_to_array(path):
+    test_file = open(path, "r")
+    lines = test_file.read().splitlines()
+    return lines
+
+array = txt_to_array("test1.txt")
+comparison = 0
+
+def quick_sort_last_pivot(array, comparison):
+    n = len(array)
+    if n <= 1:
+        return array, 0
+    else:
+        p = array[-1]
+        comparisons = 0
+        # partition array around the p (pivot)
+        # i is set as boundary between the pivot and the rest.
+        i = 0
+        for k in range(0, n-1):
+            comparisons += 1
+            if int(p) > int(array[k]):
+                tmp = array[k]
+                array[k] = array[i]
+                array[i] = tmp
+                i += 1
+        # print('before: ' + str(comparisons) + ', ' + str(comparison))
+        # print(p)
+        # print('before: ' + str(array))
+        comparison += comparisons
+        # print('after:  ' + str(comparisons) + ', ' + str(comparison))
+        array[-1], array[i] = array[i], p
+        # print('after:  ' + str(array))
+        first, second = array[0:i], array[i+1:n]
+        first_sorted, first_comparison = quick_sort_last_pivot(first, comparison)
+        second_sorted, second_comparison = quick_sort_last_pivot(second, comparison)
+        p_array = [p]
+        return first_sorted + p_array + second_sorted, comparison
+
+print(array)
+print(quick_sort_last_pivot(array, comparison))
